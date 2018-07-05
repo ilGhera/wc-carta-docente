0ab41eeb7804bba2446178c985295a5e6e87639d
ilGhera <andrea@ilghera.com>
2018-07-05 16:07:00 +0200

    Avviso premium key in fase di aggiornaemnto plugin

--- wc-carta-docente.php
+++ wc-carta-docente.php
@@ -14,15 +14,13 @@
  */
 
 
-/*Attivazione*/
+/**
+ * Attivazione
+ */
 function wccd_premium_activation() {
 
-	if ( !function_exists( 'is_plugin_active' ) ) {
-    	require_once( ABSPATH . '/wp-admin/includes/plugin.php' );
- 	}
-
 	/*Se presente, disattiva la versione free del plugin*/
-	if(is_plugin_active('wc-carta-docente/wc-carta-docente.php') || function_exists('wccd_activation')) {
+	if(function_exists('wccd_activation')) {
 		deactivate_plugins('wc-carta-docente/wc-carta-docente.php');
 	    remove_action( 'plugins_loaded', 'wccd_activation' );
 	    wp_redirect(admin_url('plugins.php?plugin_status=all&paged=1&s'));
@@ -61,7 +59,9 @@ function wccd_premium_activation() {
 add_action('plugins_loaded', 'wccd_premium_activation', 1);
 
 
-/*Update checker*/
+/**
+ * Update checker
+ */
 require( plugin_dir_path( __FILE__ ) . 'plugin-update-checker/plugin-update-checker.php');
 $wccd_update_checker = Puc_v4_Factory::buildUpdateChecker(
     'https://www.ilghera.com/wp-update-server-2/?action=get_metadata&slug=wc-carta-docente-premium',
@@ -78,3 +78,46 @@ function wccd_secure_update_check($queryArgs) {
     }
     return $queryArgs;
 }
+
+
+/**
+ * Avvisi utente in fase di aggiornaemnto plugin
+ */
+function wccd_update_message( $plugin_data, $response) {
+
+	$message = null;
+	$key = get_option('wccd-premium-key');
+
+    $message = null;
+
+	if(!$key) {
+
+		$message = __('Per ricevere aggiornamenti devi inserire la tua <b>Premium Key</b> nelle <a href="' . admin_url() . 'admin.php/?page=wccd-settings">impostazioni del plugin</a>. Clicca <a href="https://www.ilghera.com/product/woocommerce-carta-docente-premium/" target="_blank">qui</a> per maggiori informazioni.', 'wccd');
+	
+	} else {
+	
+		$decoded_key = explode('|', base64_decode($key));
+	    $bought_date = date( 'd-m-Y', strtotime($decoded_key[1]));
+	    $limit = strtotime($bought_date . ' + 365 day');
+	    $now = strtotime('today');
+
+	    if($limit < $now) { 
+	        $message = __('Sembra che la tua <strong>Premium Key</strong> sia scaduta. Clicca <a href="https://www.ilghera.com/product/woocommerce-carta-docente-premium/" target="_blank">qui</a> per maggiori informazioni.', 'wccd');
+	    } elseif($decoded_key[2] != 3518) {
+	    	$message = __('Sembra che la tua <strong>Premium Key</strong> non sia valida. Clicca <a href="https://www.ilghera.com/product/woocommerce-carta-docente-premium/" target="_blank">qui</a> per maggiori informazioni.', 'wccd');
+	    }
+
+	}
+
+	$allowed = array(
+		'b' => array(),
+		'a' => array(
+			'href'   => array(),
+			'target' => array()
+		),
+	);
+
+	echo ($message) ? '<br><span class="wccd-alert">' . wp_kses($message, $allowed) . '</span>' : '';
+
+}
+add_action('in_plugin_update_message-wc-carta-docente-premium/wc-carta-docente.php', 'wccd_update_message', 10, 2);
