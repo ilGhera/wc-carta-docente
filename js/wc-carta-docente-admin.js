e976d7d10ea7ddab2cc2ef119a2022b622824e3e
ilGhera <andrea@ilghera.com>
2018-11-08 13:05:52 +0100

    Possibilità di abbinare più beni alla stessa categoria wp Aggiunta nuova tipologia bene

--- js/wc-carta-docente-admin.js
+++ js/wc-carta-docente-admin.js
@@ -45,12 +45,6 @@ var add_cat = function() {
 				beni_values.push($(this).val());
 			})
 
-			/*Categorie già utilizzate da escludere*/
-			var cats_values = [];
-			$('.wccd-field.categories').each(function(){
-				cats_values.push($(this).val());
-			})
-
 			/*Se assegnate tutte le categorie visualizza messaggio*/
 			if(number > limit) {
 				alert('Tutte le categorie di prodotto sono state assegnate.');
@@ -60,7 +54,6 @@ var add_cat = function() {
 					'action': 'add-cat',
 					'number': number,
 					'exclude-beni': beni_values.toString(),
-					'exclude-cats': cats_values.toString()
 				}
 				$.post(ajaxurl, data, function(response){
 					$(response).appendTo('.categories-container');
